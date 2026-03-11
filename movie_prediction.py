a95fb16a5aa43023b7bd1862b9cc6178771e05ba
Maraganikuntla Shaik Mohammed Numan <mnumanvig@gmail.com>
2026-03-11 17:08:38 +0530

    Add matplotlib for data visualization

--- movie_prediction.py
+++ movie_prediction.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.preprocessing import StandardScaler
 import scipy.cluster.hierarchy as sch
