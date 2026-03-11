2b8913906ba54ff1f4b7a057dc08156701143e6e
Maraganikuntla Shaik Mohammed Numan <mnumanvig@gmail.com>
2026-03-11 16:55:14 +0530

    Remove unused matplotlib import
    Removed matplotlib import as it is not used.

--- movie_prediction.py
+++ movie_prediction.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.preprocessing import StandardScaler
 import scipy.cluster.hierarchy as sch
